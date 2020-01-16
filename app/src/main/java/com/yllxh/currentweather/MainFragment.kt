88c81c9ebacdb817a5267a7b6b3aa25eeeb13626
yllxh <yllxhd@gmail.com>
2020-01-16 03:44:31 +0100

    Added NetworkAlerter class enable the app to listen to network state changes.

--- app/src/main/java/com/yllxh/currentweather/MainFragment.kt
+++ app/src/main/java/com/yllxh/currentweather/MainFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.yllxh.currentweather.utils.NetworkAlerter
+import com.yllxh.currentweather.utils.NetworkState
 
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), NetworkAlerter.NetworkStateListener {
 
     private val viewModel  by lazy {
         ViewModelProviders.of(this).get(MainViewModel::class.java)
@@ -18,7 +21,21 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        NetworkAlerter.setListener(this, requireContext())
+
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onNetworkStateChanged(state: NetworkState) {
+        val ms = when (state) {
+            NetworkState.AVAILABLE -> "Connected"
+            else -> "Lost Connection"
+        }
+        toast(ms)
+    }
+
+}
+
+fun Fragment.toast(ms: String, length: Int = Toast.LENGTH_SHORT){
+    Toast.makeText(requireContext(), ms, length).show()
 }
---
+++ app/src/main/java/com/yllxh/currentweather/utils/NetworkAlerter.kt
@@ -0,0 +1,70 @@
+package com.yllxh.currentweather.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+
+enum class NetworkState { AVAILABLE, LOST }
+
+class NetworkAlerter private constructor(
+    private val listener: NetworkStateListener,
+    private val context: Context,
+    lifecycle: Lifecycle
+) : LifecycleObserver {
+
+    interface NetworkStateListener {
+        fun onNetworkStateChanged(state: NetworkState)
+    }
+
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    private var connectivityManager: ConnectivityManager = getConnectivityManager()
+
+    init {
+        lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun startLisening() {
+        initNetworkCallback()
+        registerNetworkCallback()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun stopListening() = connectivityManager.unregisterNetworkCallback(networkCallback)
+
+    private fun registerNetworkCallback() {
+        connectivityManager.registerNetworkCallback(
+            NetworkRequest.Builder().build(),
+            networkCallback
+        )
+    }
+
+    private fun initNetworkCallback() {
+        networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                listener.onNetworkStateChanged(NetworkState.AVAILABLE)
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                listener.onNetworkStateChanged(NetworkState.LOST)
+            }
+        }
+    }
+
+    private fun getConnectivityManager() =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    companion object {
+        fun <T>setListener(listener: T, context: Context) where T: LifecycleOwner, T: NetworkStateListener{
+            NetworkAlerter(listener, context, listener.lifecycle)
+        }
+    }
+
+}
